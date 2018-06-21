bb0628e35df077950f8c15dea78199c9e512b7e5
KuroMaju <reimicarter@gmail.com>
2018-06-21 16:35:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Code your solutions in this file
+function tailsNeverFails(){
+  let tails = 0;
+  
+  while (Math.random() >= 0.5){
+    
+  }
+  console.log(`you got ${tails} tails in a row!`);
+  
+}
